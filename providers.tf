2ecaa8cc9ca72921790cf41bf76b81e13d7ce3e8
Alvin <alvinkhamron@gmail.com>
2024-01-22 13:04:21 +0300

    [Project init]

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+  default_tags {
+    tags = {
+      Owner = "alvin"
+      ManagedBy = "opentofu"
+    }
+  }
+}
